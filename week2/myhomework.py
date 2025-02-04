e966eb5b68028dfaf778f1f3597898b74376504d
ski <kacoroski@gmail.com>
2025-02-04 12:09:09 +0900

    cleanup

--- week2/myhomework.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-# Find day of your birth
-# Licensed under the MIT License (https://opensource.org/license/mit)
-# Ski-20250203: initial version
-
-# Set up initial variables and imports
-import csv
